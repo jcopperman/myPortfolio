fecd0f9f427874c6807dfd2e670e0ca784571a71
Jonathan Opperman <jonathan.opperman@thesustainables.nl>
2022-12-19 00:53:31 +0200

    Hard coded blog post

--- src/portfolio.js
+++ src/portfolio.js
@@ -359,20 +359,13 @@ const blogSection = {
   subtitle:
     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
-  // blogs: [
-  //   {
-  //     url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-  //     title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-  //     description:
-  //       "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-  //   },
-  //   {
-  //     url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-  //     title: "Why REACT is The Best?",
-  //     description:
-  //       "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-  //   }
-  // ],
+  blogs: [
+    {
+      url: "https://medium.com/@opesinorbit/the-end-to-end-saga-cfa870d2d80f",
+      title: "The end to end saga",
+      description: "End to end testing is such a dug up dead horse that nobody wants to talk about anymore and for good reason: It's a moot topic."
+    }
+  ],
   display: true // Set false to hide this section, defaults to true
 };
 
